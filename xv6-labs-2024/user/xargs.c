cab006ea59147a12286433252aba4321553d74f4
Hector <hectorgetfree@gmail.com>
2025-07-07 20:33:20 +0800

    second try for xargs

--- xv6-labs-2024/user/xargs.c
+++ xv6-labs-2024/user/xargs.c
@@ -5,42 +5,60 @@
 #include "kernel/fcntl.h"
 #include "param.h"
 
-/*
-    要编写 xargs
-    主要就是进行指令执行，和字符串的的重新拼接得到新指令
-*/
-
-char* xargs(char* cmd1, char* cmd1_args[], char* cmd2) {
-    char* buf[1];
-    int p[2];
-    pipe(p);
-
-    // 先运行cmd1并储存其运行结果
-    if (fork() == 0) {
-        // 子进程执行cmd1
-        close(1); // 关闭标准输出
-        dup(p[1]); // 将输出重定向到写端
-        exec(cmd1, cmd1_args);
-        close(p[0]);
-        exit(0);
-    } else {
-        wait(0);
-        // 将结果读取到buf
-        read(p[0], buf, 1);
-        // 处理buf
-
-        // 拼接字符串
-
-        close(p[0]);
-        close(p[1]);
-        return cmd2;
+int read_line(char* new_argv[], int current_arg) {
+    char buf[1024];
+    int n = 0;
+    // 逐字节读取
+    while (read(0, buf+n, 1) == 1) {
+        if (buf[n] == '\n') {
+            break;
+        }
+        n++;
     }
+
+    if (n == 0) {
+        // 遇到空行或者出错
+        return 0;
+    }
+
+    int offset = 0;
+    while (offset < n) {
+        current_arg++;
+        while (buf[offset] != ' ' && offset < n) {
+            continue;
+        } 
+        while (buf[offset] == ' ' && offset < n) {
+            buf[offset] = 0;
+        }
+        offset++;
+    }
+
+    return current_arg;
 }
 
-int main() {
-    // 读取指令
-    // 分割
-    // 传参
-    // 运行
-    
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: xargs command (arg ...)\n");
+        exit(1);
+    }
+
+    char* cmd = argv[1];
+
+    // 获取新的参数列表
+    char* new_argv[MAXARG];
+    for (int i = 2; i < argc; i++) {
+        new_argv[i - 2] = argv[i];
+    }
+
+    // 处理参数列表并执行
+    int current_arg = 0;
+    while ((current_arg = read_line(new_argv, current_arg)) != 0) {
+        new_argv[current_arg] = 0;
+        if (fork() == 0) {
+            exec(cmd, new_argv);
+            exit(0);
+        }
+        wait(0);
+    }
+    exit(0);
 }
